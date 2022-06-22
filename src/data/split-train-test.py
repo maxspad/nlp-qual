08d3cb6e72a4f3018a75532c2faaa6a27d520d4f
Maxwell Spadafore <maxspad@umich.edu>
2022-06-21 20:54:59 -0400

    split-train-test.py

---
+++ src/data/split-train-test.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import typer
+import logging as log
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+def main(dataset_path: Path = typer.Option('data/processed/masterdbForNLP.xlsx', exists=True, dir_okay=False),
+         output_dir: Path = typer.Option('data/processed/', dir_okay=True),
+         test_size: float = 0.15,
+         random_state: int = 43,
+         train_file_name: str = 'train.csv',
+         test_file_name: str = 'test.csv',
+         log_level: str = typer.Option('INFO')):
+
+    params = locals()
+    log.basicConfig(level=log_level)
+    log.info("Splitting datset into train and test...")
+    log.debug(f"Parameters:\n{params}")
+
+    log.info(f'Loading data from {dataset_path}')
+    df = pd.read_excel(dataset_path)
+
+    log.info(f'Setting aside {test_size} of data for final testing')
+    train, test = train_test_split(df, test_size=test_size, random_state=random_state)
+    log.info(f'Train shape {train.shape} / Test shape {test.shape}')
+    
+    train_path = output_dir/train_file_name
+    test_path = output_dir/test_file_name
+    log.info(f'Saving train file to {train_path}')
+    train.to_csv(train_path)
+    log.info(f'Saving test file to {test_path}')
+    test.to_csv(test_path)
+
+if __name__ == '__main__':
+    typer.run(main)
