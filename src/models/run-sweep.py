ce2cbf3bd4a84de9d5e139c00979c14e134e3ac8
Maxwell Spadafore <maxspad@umich.edu>
2022-06-30 14:52:01 -0400

    added helper script run-sweep.py

---
+++ src/models/run-sweep.py
@@ -0,0 +1,24 @@
+import subprocess
+import typer
+from typing import Optional, List
+import datetime
+import os
+
+def main(sweep_name: str = typer.Argument('sweep'),
+         append_time: bool = True,
+         hydra_args: Optional[List[str]] = typer.Option(None, '-a', '--arg')):
+
+
+    now = datetime.datetime.now()
+    now_str = now.strftime('%y%m%d_%H%M%S')
+    exp_name = sweep_name if not append_time else f'{now_str}_{sweep_name}'
+    fullcmd = ['python', '-m', 'src.models.train', '--multirun', f'train.mlflow_experiment_name={exp_name}']
+    fullcmd += hydra_args
+
+    print('Running:')
+    print(' '.join(fullcmd))
+
+    subprocess.call(fullcmd)
+
+if __name__ == '__main__':
+    typer.run(main)
